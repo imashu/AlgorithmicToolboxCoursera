f983757c311d055aca65db2458fef2d8a75b0dc2
ASHUTOSH KUMAR <debug.ashu@gmail.com>
2020-06-24 09:11:46 +0530

    uploaded week 2
    .cpp completed all assignments till week 2

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+typedef unsigned long long ll;
 int fibonacci_sum_naive(long long n) {
     if (n <= 1)
         return n;
@@ -17,9 +17,57 @@ int fibonacci_sum_naive(long long n) {
 
     return sum % 10;
 }
+ll pisanoperiod(ll m)
+{
+    ll a=0;
+    ll b=1;
+    ll c=(a+b);
+    for(ll i=0;i<m*m;i++)
+    {
+        c=(a+b)%m;
+        a=b;
+        b=c;
+        if(a==0 && b==1)
+            return i+1;
+    }
+}
+
+
+ll get_fibonacci_huge_fast(ll n,ll m) {
+    // write your code here
+    ll rem=n%pisanoperiod(m);
+    ll f=0,s=1;
+    ll res=rem;
+    for(int i=1;i<rem;i++)
+    {
+        res=(f+s)%m;
+        f=s;
+        s=res;
+    }
+    return res%m;
+
+}
+
+/* f(0)+f(1)+F(2)+F(3)+F(4)+........F(n)=f(n+2)-F(2)   */
+
+
+
+int fibonacci_sum_fast(long long n) {
+
+
+
+        int ld_nplus2=get_fibonacci_huge_fast(n+2,10);
+
+        if(ld_nplus2>=1)
+            return (ld_nplus2-1);
+        else
+            return 9;
+
+
+}
 
 int main() {
-    long long n = 0;
+    long long n;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << fibonacci_sum_fast(n);
 }
